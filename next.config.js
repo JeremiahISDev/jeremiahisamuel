7a7438abf841db2656412b018a9a4d311b93e3bd
Jeremiah Ihab Samuel <79987177+JeremiahISamuelMMS@users.noreply.github.com>
2022-02-01 04:12:19 +0000

    fndfskjdfskhjfdsj

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/Movies/Soap',
-        destination: 'https://soap2day.rs/',
-      },
-      {
-        source: '/Movies/Stream',
-        destination: 'https://stream-planet.com/',
-      }
-    ]
-  },
+
 };

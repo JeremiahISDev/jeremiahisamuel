c07b78e6f8d69a09c330bd0e77502394c425358a
Jeremiah Ihab Samuel <79987177+JeremiahISamuelMMS@users.noreply.github.com>
2022-02-02 12:51:18 +0000

    fx

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import "prismjs/themes/prism-tomorrow.css";
 export default function Portfolio({ Component, pageProps }) {
   return (
     <>
-    <Script id="luckyOrange" async defer src="https://tools.luckyorange.com/core/lo.js?site-id=605447fe">
+    <Script id="luckyOrange" src="https://tools.luckyorange.com/core/lo.js?site-id=605447fe">
         </Script>
 <script src="../components/inspectlet.js">
 </script>

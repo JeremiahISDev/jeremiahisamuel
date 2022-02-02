edf17a92496cb49268673a246b0f1d83a72229ce
Jeremiah Ihab Samuel <79987177+JeremiahISamuelMMS@users.noreply.github.com>
2022-02-02 12:48:32 +0000

    inspectlet

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,17 @@
 import '../styles/globals.css'
 import Navigation from '../components/Navigation'
 import Head from 'next/head'
+import Script from 'next/script';
 import "prismjs/themes/prism-tomorrow.css";
 export default function Portfolio({ Component, pageProps }) {
   return (
     <>
+    <Script id="luckyOrange" async defer src="https://tools.luckyorange.com/core/lo.js?site-id=605447fe">
+        </Script>
+<script src="../components/inspectlet.js">
+</script>
       <Head>
-        <script async defer src="https://tools.luckyorange.com/core/lo.js?site-id=605447fe">
-        </script>
+        
        <meta name="google-site-verification" content="GvQClaWf6xn3H-Pf6ytkMkBpf8J1jjTNb_sEMVpHyKE" />
       </Head>
       <Navigation />

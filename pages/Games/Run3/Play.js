38ece65145ed0a9f57129dc85912bfff78ea75ae
JeremiahISDev <79987177+JeremiahISDev@users.noreply.github.com>
2022-09-21 07:48:57 -0400

    Create Play.js

---
+++ pages/Games/Run3/Play.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+export default function Game(){
+  return (
+    <>
+<Head>
+<title>Run 3 | JIS Media Pro</title>
+</Head>
+    <iframe width="100%" height="100%" frameBorder="0" src="https://mj89sp3sau2k7lj1eg3k40hkeppguj6j-a-sites-opensocial.googleusercontent.com/gadgets/ifr?url=http://www.gstatic.com/sites-gadgets/iframe/iframe.xml&container=enterprise&view=default&lang=en&country=ALL&sanitize=0&v=e837c6a9a9ef80ee&libs=core&mid=175&parent=https://sites.google.com/site/unblockedgame76/run3#up_scroll=no&up_iframeURL=https://lekug.github.io/tn6pS9dCf37xAhkJv/&st=e%3DAIHE3cAU7THxIjeMdgU7EO%252FpmbszYDiz1reiBj%252Bf%252B4p2MKgTYYTOnL2BSOyhzHTODAeOkZpd6wk%252B1tbydAduZu96ZetEtb3CbqyNW5Y7bwjrjIocXxVT2M1IQQxMKVoCmK56cbPHwjYq%26c%3Denterprise&rpctoken=-5609303291146068873" scrolling="yes" seamless="seamless" className="h-screen m-0 p-0 absolute"></iframe>
+    </>
+  )
+}
